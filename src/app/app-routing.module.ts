5c1d9da8ca4dab461c92d116015cefedf16d0bf2
gontsejnr <gontsemaepa@gmail.com>
2019-07-01 14:15:10 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { BookingComponent } from './booking/booking.component';
 
-const routes: Routes = [{path:"", component:HomeComponent},{path:"booking", component:BookingComponent}];
+const routes: Routes = [{path:"", component:HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
